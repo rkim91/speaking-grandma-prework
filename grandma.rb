adce03f69da50621c61a01a8857732124895eb96
Richard Kim <richeekim@gmail.com>
2018-03-23 01:16:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(speak_to_grandma)
   if speak_to_grandma == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif speak_to_grandma.upcase
-    "NO, NOT SINCE 1938!"
-  elsif speak_to_grandma
-    "HUH?! SPEAK UP, SONNY!"
+  # elsif speak_to_grandma.upcase
+  #   "NO, NOT SINCE 1938!"
+  # elsif speak_to_grandma
+  #   "HUH?! SPEAK UP, SONNY!"
   end
 end
 
