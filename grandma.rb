2276ecebb247545964a81a7ae5c03abe7e184d35
Richard Kim <richeekim@gmail.com>
2018-03-23 01:13:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(speak_to_grandma)
     "I LOVE YOU TOO PUMPKIN!"
   elsif speak_to_grandma.upcase
     "NO, NOT SINCE 1938!"
-  else
+  else speak_to_grandma.downcase
     "HUH?! SPEAK UP, SONNY!"
   end
 end
